f52873519d6f460c9dccdae2aed08ecc310d0a93
Ayesha Fawad <ayesha345@gmail.com>
2013-10-29 22:24:24 -0400

    authentication with image analysis WPF

---
+++ Photo-Rainbow/ImageAnalysis.xaml.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+//using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Drawing;
+
+namespace Photo_Rainbow
+{
+    /// <summary>
+    /// Interaction logic for ImageAnalysis.xaml
+    /// </summary>
+    public partial class ImageAnalysis : Window
+    {
+        public ImageAnalysis()
+        {
+            InitializeComponent();           
+        }
+
+        private Label label1 = new Label();
+        private Label label2 = new Label();
+        private Label label3 = new Label();
+        private Label label4 = new Label();
+        private Label label5 = new Label();
+        private Label label6 = new Label();
+        private Label label7 = new Label();
+        private Label label8 = new Label();
+        private Label label9 = new Label();
+        private Label label10 = new Label();
+        private Label label11 = new Label();
+        private Label label12 = new Label();
+        private Label label13 = new Label();
+        private Label label14 = new Label();
+        private Label label15 = new Label();
+        private Label label16 = new Label();
+        private Label label17 = new Label();
+        private Label label18 = new Label();
+        private Label label19 = new Label();
+        private Label label20 = new Label();
+        private Label label21 = new Label();
+        private Label label22 = new Label();
+        private Label label23 = new Label();
+
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                ImageColorData iCD = new ImageColorData();
+                ImageAnalysis ia = new ImageAnalysis();
+                Bitmap img = new Bitmap("C:\\Users\\Administrator\\Desktop\\1.jpg");
+                //( new Uri("Images/1.jpg", UriKind.Relative).AbsolutePath);
+                Dictionary<String, List<Color>> colorStructsOfImage = iCD.getColorsInImage(img);
+                float avgBrightnessByColor = iCD.calcAverageBrightnessByColor("Yellow");
+                float percentageOfColor = iCD.percentageOfColorInImage("Yellow");
+                label8.Content = "PERCENTAGE BY COLOR";
+                label1.Content = "Violet     " + iCD.percentageOfColorInImage("Violet").ToString();
+                label2.Content = "Indigo     " + iCD.percentageOfColorInImage("Indigo").ToString();
+                label3.Content = "Blue     " + iCD.percentageOfColorInImage("Blue").ToString();
+                label4.Content = "Green     " + iCD.percentageOfColorInImage("Green").ToString();
+                label5.Content = "Yellow     " + iCD.percentageOfColorInImage("Yellow").ToString();
+                label6.Content = "Orange     " + iCD.percentageOfColorInImage("Orange").ToString();
+                label7.Content = "Red     " + iCD.percentageOfColorInImage("Red").ToString();
+                label9.Content = "BRIGHTNESS BY COLOR";
+                label17.Content = "Violet     " + iCD.calcAverageBrightnessByColor("Violet").ToString();
+                label18.Content = "Indigo     " + iCD.calcAverageBrightnessByColor("Indigo").ToString();
+                label19.Content = "Blue     " + iCD.calcAverageBrightnessByColor("Blue").ToString();
+                label20.Content = "Green     " + iCD.calcAverageBrightnessByColor("Green").ToString();
+                label21.Content = "Yellow     " + iCD.calcAverageBrightnessByColor("Yellow").ToString();
+                label22.Content = "Orange     " + iCD.calcAverageBrightnessByColor("Orange").ToString();
+                label23.Content = "Red     " + iCD.calcAverageBrightnessByColor("Red").ToString();
+
+                this.splMain.Children.Add(label8);
+                this.splMain.Children.Add(label1);
+                this.splMain.Children.Add(label2);
+                this.splMain.Children.Add(label3);
+                this.splMain.Children.Add(label4);
+                this.splMain.Children.Add(label5);
+                this.splMain.Children.Add(label6);
+                this.splMain.Children.Add(label7);
+                this.splMain.Children.Add(label9);
+                this.splMain.Children.Add(label17);
+                this.splMain.Children.Add(label18);
+                this.splMain.Children.Add(label19);
+                this.splMain.Children.Add(label20);
+                this.splMain.Children.Add(label21);
+                this.splMain.Children.Add(label22);
+                this.splMain.Children.Add(label23);
+
+            }
+
+            catch (Exception ee)
+            {
+
+            }
+
+        }
+        
+    }
+}
--- Photo-Rainbow/imageColorData.cs
+++ Photo-Rainbow/imageColorData.cs
@@ -9,10 +9,11 @@ namespace Photo_Rainbow
 {
     class ImageColorData
     {
-        private int imageWidth;
-        private int imageHeight;
-        private Dictionary<String, float> _brightnessColorDict;
-        private Dictionary<String, List<Color>> _colorByPixel;
+        private int imageWidth = 0;
+        private int imageHeight = 0;
+        private Dictionary<String, float> _brightnessColorDict = new Dictionary<string, float>();
+        private Dictionary<String, List<Color>> _colorByPixel = new Dictionary<string, List<Color>>();
+
 
         public Dictionary<String, float> brightnessColorDict
         {
@@ -30,94 +31,117 @@ namespace Photo_Rainbow
             float temp = 0;            
             this.imageWidth = imageAsBitmap.Width;
             this.imageHeight = imageAsBitmap.Height;
-                
-            for (xCoord = 0; xCoord < this.imageWidth; xCoord++)
+
+            try
             {
-                for (yCoord = 0; yCoord < this.imageHeight; yCoord++)
+                for (xCoord = 0; xCoord < this.imageWidth; xCoord++)
                 {
-                    Dictionary<float, String> imgVIBGYORHueDiffDict = new Dictionary<float, string>();
-                    List<float> imgVIBGYORHueDiff = new List<float>();
-                    Color imgPixelColor = imageAsBitmap.GetPixel(xCoord, yCoord);
-                    float pixelColorHue = imgPixelColor.GetHue();
-                    temp = pixelColorHue - Color.Violet.GetHue();
-                    temp = adjustHue(temp);
-                    imgVIBGYORHueDiffDict.Add(temp, "Violet");
+                    for (yCoord = 0; yCoord < this.imageHeight; yCoord++)
+                    {
+                        Dictionary<float, String> imgVIBGYORHueDiffDict = new Dictionary<float, string>();
+                        List<float> imgVIBGYORHueDiff = new List<float>();
+                        Color imgPixelColor = imageAsBitmap.GetPixel(xCoord, yCoord);
+                        float pixelColorHue = imgPixelColor.GetHue();
+                        temp = pixelColorHue - Color.Violet.GetHue();
+                        temp = adjustHue(temp);
+                        imgVIBGYORHueDiffDict.Add(temp, "Violet");
 
-                    temp = pixelColorHue - Color.Indigo.GetHue();
-                    temp = adjustHue(temp);
-                    if (!imgVIBGYORHueDiffDict.ContainsKey(temp))
-                    {
-                        imgVIBGYORHueDiffDict.Add(temp, "Indigo");
-                    }
+                        temp = pixelColorHue - Color.Indigo.GetHue();
+                        temp = adjustHue(temp);
+                        if (!imgVIBGYORHueDiffDict.ContainsKey(temp))
+                        {
+                            imgVIBGYORHueDiffDict.Add(temp, "Indigo");
+                        }
 
-                    temp = pixelColorHue - Color.Blue.GetHue();
-                    temp = adjustHue(temp);
-                    if (!imgVIBGYORHueDiffDict.ContainsKey(temp))
-                    {
-                        imgVIBGYORHueDiffDict.Add(temp, "Blue");
-                    }
-                        
-                    temp = pixelColorHue - Color.Green.GetHue();
-                    temp = adjustHue(temp);
-                    if (!imgVIBGYORHueDiffDict.ContainsKey(temp))
-                    {
-                        imgVIBGYORHueDiffDict.Add(temp, "Green");
-                    }
-                        
-                    temp = pixelColorHue - Color.Yellow.GetHue();
-                    temp = adjustHue(temp);
-                    if (!imgVIBGYORHueDiffDict.ContainsKey(temp))
-                    {
-                        imgVIBGYORHueDiffDict.Add(temp, "Yellow");
-                    }
+                        temp = pixelColorHue - Color.Blue.GetHue();
+                        temp = adjustHue(temp);
+                        if (!imgVIBGYORHueDiffDict.ContainsKey(temp))
+                        {
+                            imgVIBGYORHueDiffDict.Add(temp, "Blue");
+                        }
 
-                    temp = pixelColorHue - Color.Orange.GetHue();
-                    temp = adjustHue(temp);
-                    if (!imgVIBGYORHueDiffDict.ContainsKey(temp))
-                    {
-                        imgVIBGYORHueDiffDict.Add(temp, "Orange");
-                    }
+                        temp = pixelColorHue - Color.Green.GetHue();
+                        temp = adjustHue(temp);
+                        if (!imgVIBGYORHueDiffDict.ContainsKey(temp))
+                        {
+                            imgVIBGYORHueDiffDict.Add(temp, "Green");
+                        }
 
-                    temp = pixelColorHue - Color.Red.GetHue();
-                    temp = adjustHue(temp);
-                    if (!imgVIBGYORHueDiffDict.ContainsKey(temp))
-                    {
-                        imgVIBGYORHueDiffDict.Add(temp, "Red");
+                        temp = pixelColorHue - Color.Yellow.GetHue();
+                        temp = adjustHue(temp);
+                        if (!imgVIBGYORHueDiffDict.ContainsKey(temp))
+                        {
+                            imgVIBGYORHueDiffDict.Add(temp, "Yellow");
+                        }
+
+                        temp = pixelColorHue - Color.Orange.GetHue();
+                        temp = adjustHue(temp);
+                        if (!imgVIBGYORHueDiffDict.ContainsKey(temp))
+                        {
+                            imgVIBGYORHueDiffDict.Add(temp, "Orange");
+                        }
+
+                        temp = pixelColorHue - Color.Red.GetHue();
+                        temp = adjustHue(temp);
+                        if (!imgVIBGYORHueDiffDict.ContainsKey(temp))
+                        {
+                            imgVIBGYORHueDiffDict.Add(temp, "Red");
+                        }
+                        foreach (float hueDiff in imgVIBGYORHueDiffDict.Keys)
+                        {
+                            imgVIBGYORHueDiff.Add(hueDiff);
+                        }
+                        float closestPixelColorByHue = imgVIBGYORHueDiff.Min();
+                        if (_colorByPixel.ContainsKey(imgVIBGYORHueDiffDict[closestPixelColorByHue]))
+                        {
+                            _colorByPixel[imgVIBGYORHueDiffDict[closestPixelColorByHue]].Add(imgPixelColor);
+                            _brightnessColorDict[imgVIBGYORHueDiffDict[closestPixelColorByHue]] += imgPixelColor.GetBrightness();
+                        }
+                        else
+                        {
+                            List<Color> pixelColorStructure = new List<Color>();
+                            pixelColorStructure.Add(imgPixelColor);
+                            _colorByPixel.Add(imgVIBGYORHueDiffDict[closestPixelColorByHue], pixelColorStructure);
+                            _brightnessColorDict.Add(imgVIBGYORHueDiffDict[closestPixelColorByHue], imgPixelColor.GetBrightness());
+                        }
                     }
-                    foreach (float hueDiff in imgVIBGYORHueDiffDict.Keys)
-                    {
-                        imgVIBGYORHueDiff.Add(hueDiff);
-                    }
-                    float closestPixelColorByHue = imgVIBGYORHueDiff.Min();
-                    if (_colorByPixel.ContainsKey(imgVIBGYORHueDiffDict[closestPixelColorByHue]))
-                    {
-                        _colorByPixel[imgVIBGYORHueDiffDict[closestPixelColorByHue]].Add(imgPixelColor);
-                        _brightnessColorDict[imgVIBGYORHueDiffDict[closestPixelColorByHue]] += imgPixelColor.GetBrightness();
-                    }
-                    else
-                    {
-                        List<Color> pixelColorStructure = new List<Color>();
-                        pixelColorStructure.Add(imgPixelColor);
-                        _colorByPixel.Add(imgVIBGYORHueDiffDict[closestPixelColorByHue], pixelColorStructure);                        
-                        _brightnessColorDict.Add(imgVIBGYORHueDiffDict[closestPixelColorByHue], imgPixelColor.GetBrightness()); 
-                    }                                     
                 }
+            }
+
+            catch (Exception e)
+            {
+                
             }
             return _colorByPixel;                
         }
 
         public float percentageOfColorInImage(String colorName)
         {
-            float numberofPixelsByColor = _colorByPixel[colorName].Count();
-            float totalPixelsInImage = imageWidth * imageHeight;
-            float percentageOfColor = (numberofPixelsByColor / totalPixelsInImage) * 100;
+            float percentageOfColor = 0;
+            try
+            {
+                float numberofPixelsByColor = _colorByPixel[colorName].Count();
+                float totalPixelsInImage = imageWidth * imageHeight;
+                percentageOfColor = (numberofPixelsByColor / totalPixelsInImage) * 100;
+            }
+            catch (Exception e)
+            {
+
+            }
             return percentageOfColor;
         }
         public float calcAverageBrightnessByColor(String colorName)
         {
             float averageBrightnessByColor = 0;
+            try
+            {
             int numberOfPixelsByColor = _colorByPixel[colorName].Count();
             averageBrightnessByColor = _brightnessColorDict[colorName] / numberOfPixelsByColor;
+            }
+            catch (Exception e)
+            {
+
+            }
             return averageBrightnessByColor;
         }
         private static float adjustHue(float temp)
@@ -131,14 +155,6 @@ namespace Photo_Rainbow
             {
                 return temp;
             }
-        }
-
-        public void imageColorData()
-        {
-            imageWidth = 0;
-            imageHeight = 0;
-            _brightnessColorDict = new Dictionary<string, float>();
-            _colorByPixel = new Dictionary<string, List<Color>>();
         }
     }
 }
