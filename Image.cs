0a0283cd539f89a5ef3a7d2fc9fb0774813f9e95
mathu <mathura1987@gmail.com>
2013-11-19 10:20:05 -0500

    Add these files to the photo_Rainbow

---
+++ Image.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Net;
+using System.IO;
+
+namespace Photo_Rainbow
+{
+    public class Image
+    {
+        public static string url;
+        private static Bitmap img;
+        private ImageColorData colData;
+        private String colName;
+
+        public Image(string u)
+        {
+            url = u;
+            Download();
+            colName = "";
+            colData = new ImageColorData(img);
+        }
+
+        public void Download()
+        {
+            WebClient client = new WebClient();
+            Stream stream = client.OpenRead(url);
+
+            try
+            {
+                img = new Bitmap(stream);
+                stream.Flush();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                stream.Close();
+            }
+        }
+
+        //TODO: Create accessor for color data
+
+        public string Url
+        {
+            set { url = value; }
+            get { return url; }
+        }
+
+        public Bitmap Img
+        {
+            set { img = value; }
+            get { return img; }
+        }
+
+        public String colorName
+        {
+            set { colName = value; }
+            get { return colName; }
+        }
+        public float brightnessOfColor
+        {
+            get { return colData.calcAverageBrightnessByColor(colName); }            
+        }
+
+        public float percentageOfColor
+        {
+            get { return colData.percentageOfColorInImage(colName); }  
+        }
+
+    }
+}
