36325b8588b0e100692f39442ed00db3e629fce4
Ayesha Fawad <ayesha345@gmail.com>
2013-12-01 23:09:20 -0500

    addition to unit tests

--- UnitTests/FlickrManagerTests.cs
+++ UnitTests/FlickrManagerTests.cs
@@ -13,11 +13,14 @@ namespace UnitTests
     {
         private static FlickrManager f;
         private static Mock<Flickr> flickrMock;
+        private static Mock<FlickrManager> fMock;
+        private static Mock<ImageColorData> colorRange;
 
         [SetUp]
         public void Init()
         {
             f = new FlickrManager();
+            colorRange = new Mock<ImageColorData>();
         }
 
         [Test]
@@ -43,13 +46,55 @@ namespace UnitTests
         [Test]
         public void CompleteAuthTest()
         {
-            //TODO: figure out how to test this
+            fMock.Verify(ff => ff.Authenticate(), Times.AtLeastOnce());
+            Assert.IsTrue(f.IsAuthenticated());
         }
 
         [Test]
         public void GetPhotosTest()
         {
-            //TODO: figure out how to test this 
+            Assert.IsNotNull(f.GetPhotos());
+        }
+
+        [Test]
+        public void isViolet()
+        {
+            colorRange.Verify(m => m.colorKeyPixValue.Keys.Equals("Violet") && m.colorKeyPixValue["Violet"][0] >= 300.0 && m.colorKeyPixValue["Violet"][m.colorKeyPixValue["Violet"].Count - 1] <= 360.0);
+        }
+        [Test]
+        public void isIndigo()
+        {
+            colorRange.Verify(m => m.colorKeyPixValue.Keys.Equals("Indigo") && m.colorKeyPixValue["Indigo"][0] >= 300.0 && m.colorKeyPixValue["Indigo"][m.colorKeyPixValue["Indigo"].Count - 1] <= 360.0);
+        }
+
+        [Test]
+        public void isBlue()
+        {
+            colorRange.Verify(m => m.colorKeyPixValue.Keys.Equals("Blue") && m.colorKeyPixValue["Blue"][0] >= 300.0 && m.colorKeyPixValue["Blue"][m.colorKeyPixValue["Blue"].Count - 1] <= 360.0);
+        }
+
+        [Test]
+        public void isGreen()
+        {
+            colorRange.Verify(m => m.colorKeyPixValue.Keys.Equals("Green") && m.colorKeyPixValue["Green"][0] >= 300.0 && m.colorKeyPixValue["Green"][m.colorKeyPixValue["Green"].Count - 1] <= 360.0);
+        }
+
+        [Test]
+        public void isYellow()
+        {
+            colorRange.Verify(m => m.colorKeyPixValue.Keys.Equals("Yellow") && m.colorKeyPixValue["Yellow"][0] >= 300.0 && m.colorKeyPixValue["Yellow"][m.colorKeyPixValue["Yellow"].Count - 1] <= 360.0);
+        }
+
+        [Test]
+        public void isOrange()
+        {
+            colorRange.Verify(m => m.colorKeyPixValue.Keys.Equals("Orange") && m.colorKeyPixValue["Orange"][0] >= 300.0 && m.colorKeyPixValue["Orange"][m.colorKeyPixValue["Orange"].Count - 1] <= 360.0);
+
+        }
+        [Test]
+        public void isRed()
+        {
+            colorRange.Verify(m => m.colorKeyPixValue.Keys.Equals("Red") && m.colorKeyPixValue["Red"][0] >= 300.0 && m.colorKeyPixValue["Red"][m.colorKeyPixValue["Red"].Count - 1] <= 360.0);
         }
     }
 }
