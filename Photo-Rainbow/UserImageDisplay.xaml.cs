a27a5ea13e951b23bb46ddccd7625260ea31caa9
mathu <mathura1987@gmail.com>
2013-12-02 23:19:29 -0500

    Missed files of Present the downloaded images to user commit

---
+++ Photo-Rainbow/UserImageDisplay.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Drawing;
+
+namespace Photo_Rainbow
+{
+    /// <summary>
+    /// Interaction logic for UserImageDisplay.xaml
+    /// </summary>
+    public partial class UserImageDisplay : Window
+    {
+        private List<Image> userImages;
+        public UserImageDisplay(List<Image> userImages)
+        {
+            InitializeComponent();            
+            this.userImages = userImages;
+        }
+
+        public UserImageDisplay()
+        {
+            InitializeComponent();
+        }
+
+        public void LoadImages()
+        {            
+            foreach (Image img in userImages)
+            {
+                System.Windows.Controls.Image userImage = new System.Windows.Controls.Image();
+                userImage.Height = 100;
+                userImage.Width = 100;
+
+                BitmapImage userImageSource = new BitmapImage();
+                userImageSource.BeginInit();
+                userImageSource.UriSource = new Uri(img.Url);
+                userImageSource.EndInit();
+                userImage.Source = userImageSource;
+
+                ImagesBox.Items.Add(userImage);
+            }
+        }
+    }
+}
